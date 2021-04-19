859d7fe5a78df69aa63c37a7ac1e4166e567968b
Sander Roberti <s.roberti@nki.nl>
2021-04-19 14:35:51 +0200

    New (faster) version of linERRscore

--- R/linERRscore.R
+++ R/linERRscore.R
@@ -36,6 +36,7 @@
 #' score2$U # Original score
 #' score2$U+score2$A # Firth score
 #'
+#' @importFrom data.table data.table
 #' @export
 
 
@@ -45,235 +46,184 @@ linERRscore <- function(params,data, doses, set, status, loc, ccmethod, corrvars
   data <- data[data[,set] %in% as.numeric(names(which(table(data[,set],data[,status])[,2]==1))),]
   if(ccmethod!="CL") data <- data[data[,set] %in% as.numeric(names(which(table(data[,set])>1))),]
 
-  sp <- split(data,data[,set])
-  sp <- lapply(sp, as.matrix)
+  if(ccmethod=="CL") corrvars <- NULL
 
-  if(repar==FALSE){
-    byset <- lapply(sp, function(x){
-      # x is the x-matrix for a single matched set
-      x <- as.data.frame(x)
-
-      # transform such that each row corresponds to an organ location
-      longx <- reshape(x, direction="long", varying=list(doses), v.names="dose", timevar="loc")
-
-      # construct X: a matrix with D*=d/(1+beta*d), dummies for location effects and all variables in the exponential part of the model
-      if(ccmethod=="CCAL"){
-        X <- as.matrix(cbind(D=longx$dose/(1+params[1]*longx$dose), model.matrix(~factor(loc)-1, data=longx)[,-1], longx[, names(x)[corrvars], drop=FALSE]))
-
-      } else if(ccmethod=="CL"){
-        # CL only uses locations from the case patient
-        longx <- longx[longx[,names(data)[status]]==1,]
-        X <- as.matrix(cbind(D=longx$dose/(1+params[1]*longx$dose), model.matrix(~factor(loc)-1, data=longx)[,-1]))
-
-      } else if(ccmethod%in%c("CCML")){
-        # CCML only uses the tumor location
-        longx <- longx[longx$loc==longx[,names(data)[loc]],]
-        X <- as.matrix(cbind(D=longx$dose/(1+params[1]*longx$dose), longx[, names(x)[corrvars], drop=FALSE]))
-
-      } else if(ccmethod=="meandose"){
-        # Mean dose only uses the mean organ dose
-        longx <- cbind(x[, set, drop = FALSE], data.frame(dose = rowMeans(x[, doses])), x[, -c(set, doses), drop = FALSE])
-        X <- as.matrix(cbind(D = rowMeans(x[, doses])/(1 + params[1] * rowMeans(x[, doses])), x[, names(x)[corrvars], drop = FALSE]))
-      }
-
-      # vector of tumor location probabilities (RR_l/sum(RR))
-      p <- (1+params[1]*longx$dose)*exp(c(X[,-1,drop=FALSE]%*%params[-1]))/sum((1+params[1]*longx$dose)*exp(c(X[,-1,drop=FALSE]%*%params[-1])))
-
-      # compute S0 (scalar), SX (vector, one component for each variable), SXX (matrix) and SXXX (array) as written in manuscript. The first component is always for beta
-      S0 <- sum((1+params[1]*longx$dose)*exp(c(X[,-1,drop=FALSE]%*%params[-1])))
-      SX <- ((1+params[1]*longx$dose)*exp(c(X[,-1,drop=FALSE]%*%params[-1])))%*%X
-
-      SXX <- Reduce('+',lapply(1:nrow(longx), function(j) X[j,]%*%t(X[j,])*(1+params[1]*longx$dose[j])*exp(c(X[,-1,drop=FALSE]%*%params[-1]))[j]))
-
-      SXSX <- t(SX)%*%SX
-
-      SXXX <- array(dim=c(ncol(X),ncol(X),ncol(X)))
-      for(k in 1:ncol(X)){
-        for(s in 1:ncol(X)){
-          for(t in 1:ncol(X)){
-            SXXX[k,s,t] <- sum(X[,k]*X[,s]*X[,t]*(1+params[1]*longx$dose)*exp(c(X[,-1,drop=FALSE]%*%params[-1])))
-          }
-        }
-      }
-
-      # Expected value for X and x^2
-      EX <- p%*%X
-      EX2 <- p%*%X^2
-
-      # Components for EUU, expected value of product of 2 U's
-      tmp1 <- EX2[1]-EX[1]^2
-      tmp2 <- SXX[1,-1]/S0-SX[1]*SX[-1]/S0^2
-      tmp3 <- SXX[-1,-1]/S0- SXSX[-1,-1]/S0^2
-
-      EUU <- rbind(c(tmp1, tmp2),cbind(tmp2,tmp3))
-
-
-      # Compute EUUU (expected values of product of 3 U's) and EUU2 (expected values of product of U_a and U_{b,c})
-      # Definitions of components are given in the manuscript
-      EUUU <- array(dim=c(ncol(X),ncol(X),ncol(X)))
-      EUU2 <- array(dim=c(ncol(X),ncol(X),ncol(X)))
-      for(k in 1:ncol(X)){
-        for(s in 1:ncol(X)){
-          for(t in 1:ncol(X)){
-            if(k+s+t == 3){
-              EUUU[1,1,1] <- SXXX[1,1,1]/S0-3*(SXX[1,1]/S0)*SX[1]/S0+2*SX[1]^3/S0^3
-              EUU2[1,1,1] <- -SXXX[1,1,1]/S0+SX[1]/S0*SXX[1,1]/S0
-            } else if(k+s == 2){
-              EUUU[k,s,t] <- SXXX[k,s,t]/S0 - SXX[1,1]/S0*SX[t]/S0-2*SXX[1,t]/S0*SX[1]/S0+2*SX[1]^2*SX[t]/S0^3
-              EUU2[k,s,t] <- 0
-            } else if(k+t == 2){
-              EUUU[k,s,t] <- SXXX[k,s,t]/S0 - SXX[1,1]/S0*SX[s]/S0-2*SXX[1,s]/S0*SX[1]/S0+2*SX[1]^2*SX[s]/S0^3
-              EUU2[k,s,t] <- 0
-            } else if(s+t == 2){
-              EUUU[k,s,t] <- SXXX[k,s,t]/S0 - SXX[1,1]/S0*SX[k]/S0-2*SXX[1,k]/S0*SX[1]/S0+2*SX[1]^2*SX[k]/S0^3
-              EUU2[k,s,t] <- -SXXX[k,1,1]/S0+SX[k]/S0*SXX[1,1]/S0
-            } else if(k==1){
-              EUUU[k,s,t] <- SXXX[k,s,t]/S0-SXX[1,s]/S0*SX[t]/S0-SXX[s,t]/S0*SX[1]/S0+2*SX[1]*SX[s]*SX[t]/S0^3-SXX[1,t]/S0*SX[s]/S0
-              EUU2[k,s,t] <- 0
-            } else if(s==1){
-              EUUU[k,s,t] <- SXXX[k,s,t]/S0-SXX[1,k]/S0*SX[t]/S0-SXX[k,t]/S0*SX[1]/S0+2*SX[1]*SX[k]*SX[t]/S0^3-SXX[1,t]/S0*SX[k]/S0
-              EUU2[k,s,t] <- 0
-            } else if(t==1){
-              EUUU[k,s,t] <- SXXX[k,s,t]/S0-SXX[1,k]/S0*SX[s]/S0-SXX[k,s]/S0*SX[1]/S0+2*SX[1]*SX[k]*SX[s]/S0^3-SXX[1,s]/S0*SX[k]/S0
-              EUU2[k,s,t] <- 0
-            } else{
-              EUUU[k,s,t] <- SXXX[k,s,t]/S0-SX[s]/S0*SXX[k,t]/S0-SX[k]/S0*SXX[s,t]/S0-SX[t]/S0*SXX[k,s]/S0+2*SX[k]*SX[s]*SX[t]/S0^3
-              EUU2[k,s,t] <- 0
-            }
-          }
-        }
-      }
-
-      # Compute the score U and second derivative U2
-      if(!(ccmethod=="meandose")){
-        U <- c(X[longx[,names(data)[status]]==1 & longx[,names(data)[loc]] == longx$loc,1]-SX[1]/S0, X[longx[,names(data)[status]]==1 & longx[,names(data)[loc]] == longx$loc,-1]-SX[-1]/S0)
-        tmp1 <- -X[longx[,names(data)[status]]==1 & longx[,names(data)[loc]] == longx$loc,1]^2+SX[1]^2/S0^2
-      } else {
-        U <- c(X[longx[,names(data)[status]]==1,1]-SX[1]/S0, X[longx[,names(data)[status]]==1 ,-1]-SX[-1]/S0)
-        tmp1 <- -X[longx[,names(data)[status]]==1,1]^2+SX[1]^2/S0^2
-      }
-
-
-      tmp2 <- SX[1]*SX[-1]/S0^2-SXX[1,-1]/S0
-      tmp3 <- SXSX[-1,-1]/S0^2-SXX[-1,-1]/S0
-
-      U2 <- rbind(c(tmp1, tmp2),cbind(tmp2,tmp3))
-
-      list(S0=S0, SX=SX, SXX=SXX, SXXX=SXXX, SXSX=SXSX, EUU=EUU, EUUU=EUUU, EUU2=EUU2, U=U, U2=U2)
-    })
+  if(ccmethod %in% c("CCAL","CL")){
+    alpha <- c(0,params[2:(length(doses))])
   } else {
-    # Computation for the reparametrized setting structured equivalently to the original parametrization. See documentation above for more details
-    params[1] <- exp(params[1])
-    byset <- lapply(sp, function(x){
-      x <- as.data.frame(x)
-      longx <- reshape(x, direction="long", varying=list(doses), v.names="dose", timevar="loc")
-
-      if(ccmethod=="CCAL"){
-        X <- as.matrix(cbind(D=longx$dose/(1+params[1]*longx$dose), model.matrix(~factor(loc)-1, data=longx)[,-1], longx[, names(x)[corrvars], drop=FALSE]))
-
-      } else if(ccmethod=="CL"){
-        longx <- longx[longx[,names(data)[status]]==1,]
-        X <- as.matrix(cbind(D=longx$dose/(1+params[1]*longx$dose), model.matrix(~factor(loc)-1, data=longx)[,-1]))
-
-      } else if(ccmethod%in%c("CCML")){
-        longx <- longx[longx$loc==longx[,names(data)[loc]],]
-        X <- as.matrix(cbind(D=longx$dose/(1+params[1]*longx$dose), longx[, names(x)[corrvars], drop=FALSE]))
-
-      } else if(ccmethod=="meandose"){
-        longx <- cbind(x[, set, drop = FALSE], data.frame(dose = rowMeans(x[, doses])), x[, -c(set, doses), drop = FALSE])
-        X <- as.matrix(cbind(D = rowMeans(x[, doses])/(1 + params[1] * rowMeans(x[, doses])), x[, names(x)[corrvars], drop = FALSE]))
-      }
-
-
-      p <- (1+params[1]*longx$dose)*exp(c(X[,-1,drop=FALSE]%*%params[-1]))/sum((1+params[1]*longx$dose)*exp(c(X[,-1,drop=FALSE]%*%params[-1])))
-
-      S0 <- sum((1+params[1]*longx$dose)*exp(c(X[,-1,drop=FALSE]%*%params[-1])))
-      SX <- ((1+params[1]*longx$dose)*exp(c(X[,-1,drop=FALSE]%*%params[-1])))%*%X
-
-      SXX <- Reduce('+',lapply(1:nrow(longx), function(j) X[j,]%*%t(X[j,])*(1+params[1]*longx$dose[j])*exp(c(X[,-1,drop=FALSE]%*%params[-1]))[j]))
-
-      SXSX <- t(SX)%*%SX
-
-      SXXX <- array(dim=c(ncol(X),ncol(X),ncol(X)))
-      for(k in 1:ncol(X)){
-        for(s in 1:ncol(X)){
-          for(t in 1:ncol(X)){
-            SXXX[k,s,t] <- sum(X[,k]*X[,s]*X[,t]*(1+params[1]*longx$dose)*exp(c(X[,-1,drop=FALSE]%*%params[-1])))
-          }
-        }
-      }
-
-      EX <- p%*%X
-      EX2 <- p%*%X^2
-
-      tmp1 <- params[1]^2*(EX2[1]-EX[1]^2)
-      tmp2 <- params[1]*(SXX[1,-1]/S0-SX[1]*SX[-1]/S0^2)
-      tmp3 <- SXX[-1,-1]/S0- SXSX[-1,-1]/S0^2
-
-      EUU <- rbind(c(tmp1, tmp2),cbind(tmp2,tmp3))
-
-
-
-      EUUU <- array(dim=c(ncol(X),ncol(X),ncol(X)))
-      EUU2 <- array(dim=c(ncol(X),ncol(X),ncol(X)))
-      for(k in 1:ncol(X)){
-        for(s in 1:ncol(X)){
-          for(t in 1:ncol(X)){
-            if(k+s+t == 3){
-              EUUU[1,1,1] <- params[1]^3*(SXXX[1,1,1]/S0-3*(SXX[1,1]/S0)*SX[1]/S0+2*SX[1]^3/S0^3)
-              EUU2[1,1,1] <- params[1]^3*(-SXXX[1,1,1]/S0+SX[1]/S0*SXX[1,1]/S0)+params[1]^2*(SXX[1,1]/S0-SX[1]^2/S0^2)
-            } else if(k+s == 2){
-              EUUU[k,s,t] <- params[1]^2*(SXXX[k,s,t]/S0 - SXX[1,1]/S0*SX[t]/S0-2*SXX[1,t]/S0*SX[1]/S0+2*SX[1]^2*SX[t]/S0^3)
-              EUU2[k,s,t] <- 0
-            } else if(k+t == 2){
-              EUUU[k,s,t] <- params[1]^2*(SXXX[k,s,t]/S0 - SXX[1,1]/S0*SX[s]/S0-2*SXX[1,s]/S0*SX[1]/S0+2*SX[1]^2*SX[s]/S0^3)
-              EUU2[k,s,t] <- 0
-            } else if(s+t == 2){
-              EUUU[k,s,t] <- params[1]^2*(SXXX[k,s,t]/S0 - SXX[1,1]/S0*SX[k]/S0-2*SXX[1,k]/S0*SX[1]/S0+2*SX[1]^2*SX[k]/S0^3)
-              EUU2[k,s,t] <- params[1]^2*(-SXXX[k,s,t]/S0 + SXX[1,1]/S0*SX[k]/S0)+params[1]*(SXX[1,k]/S0-SX[1]*SX[k]/S0^2)
-            } else if(k==1){
-              EUUU[k,s,t] <- params[1]*(SXXX[k,s,t]/S0-SXX[1,s]/S0*SX[t]/S0-SXX[s,t]/S0*SX[1]/S0+2*SX[1]*SX[s]*SX[t]/S0^3-SXX[1,t]/S0*SX[s]/S0)
-              EUU2[k,s,t] <- 0
-            } else if(s==1){
-              EUUU[k,s,t] <- params[1]*(SXXX[k,s,t]/S0-SXX[1,k]/S0*SX[t]/S0-SXX[k,t]/S0*SX[1]/S0+2*SX[1]*SX[k]*SX[t]/S0^3-SXX[1,t]/S0*SX[k]/S0)
-              EUU2[k,s,t] <- 0
-            } else if(t==1){
-              EUUU[k,s,t] <- params[1]*(SXXX[k,s,t]/S0-SXX[1,k]/S0*SX[s]/S0-SXX[k,s]/S0*SX[1]/S0+2*SX[1]*SX[k]*SX[s]/S0^3-SXX[1,s]/S0*SX[k]/S0)
-              EUU2[k,s,t] <- 0
-            } else{
-              EUUU[k,s,t] <- SXXX[k,s,t]/S0-SX[s]/S0*SXX[k,t]/S0-SX[k]/S0*SXX[s,t]/S0-SX[t]/S0*SXX[k,s]/S0+2*SX[k]*SX[s]*SX[t]/S0^3
-              EUU2[k,s,t] <- 0
-            }
-          }
-        }
-      }
-
-      if(!(ccmethod=="meandose")){
-        U <- c(params[1]*(X[longx[,names(data)[status]]==1 & longx[,names(data)[loc]] == longx$loc,1]-SX[1]/S0), X[longx[,names(data)[status]]==1 & longx[,names(data)[loc]] == longx$loc,-1]-SX[-1]/S0)
-        tmp1 <- params[1]^2*(-X[longx[,names(data)[status]]==1 & longx[,names(data)[loc]] == longx$loc,1]^2+SX[1]^2/S0^2)+params[1]*(X[longx[,names(data)[status]]==1 & longx[,names(data)[loc]] == longx$loc,1]-SX[1]/S0)
-
-      } else {
-        U <- c(params[1]*(X[longx[,names(data)[status]]==1,1]-SX[1]/S0), X[longx[,names(data)[status]]==1,-1]-SX[-1]/S0)
-        tmp1 <- params[1]^2*(-X[longx[,names(data)[status]]==1,1]^2+SX[1]^2/S0^2)+params[1]*(X[longx[,names(data)[status]]==1,1]-SX[1]/S0)
-
-      }
-
-      tmp2 <- params[1]*(SX[1]*SX[-1]/S0^2-SXX[1,-1]/S0)
-      tmp3 <- SXSX[-1,-1]/S0^2-SXX[-1,-1]/S0
-
-      U2 <- rbind(c(tmp1, tmp2),cbind(tmp2,tmp3))
-
-      list(S0=S0, SX=SX, SXX=SXX, SXXX=SXXX, SXSX=SXSX, EUU=EUU, EUUU=EUUU, EUU2=EUU2, U=U, U2=U2)
-    })
+    alpha <- rep(0,length(doses))
   }
 
-  # Sum all components
-  EUU <- Reduce('+',lapply(byset, function(x) x$EUU))
-  EUUU <- Reduce('+',lapply(byset, function(x) x$EUUU))
-  EUU2 <- Reduce('+',lapply(byset, function(x) x$EUU2))
+  if(repar) params[1] <- exp(params[1])
 
-  U <- Reduce('+',lapply(byset, function(x) x$U))
-  U2 <- Reduce('+',lapply(byset, function(x) x$U2))
+  delta <- tail(params, length(corrvars))
+
+  datalong <- reshape(data, direction="long", varying=list(doses), v.names="dose", timevar="loc")
+  datalong <- datalong[order(datalong[,names(data)[set]]),]
+
+
+  if(ccmethod=="CCAL"){
+    X <- data.frame(D=datalong$dose/(1+params[1]*datalong$dose), model.matrix(~factor(loc)-1, data=datalong)[,-1], datalong[, names(data)[corrvars], drop=FALSE])
+  } else if(ccmethod=="CCML"){
+    datalong <- datalong[datalong$loc == datalong[,names(data)[loc]],]
+    X <- data.frame(D=datalong$dose/(1+params[1]*datalong$dose), datalong[, names(data)[corrvars], drop=FALSE])
+  } else if(ccmethod=="CL"){
+    datalong <- datalong[datalong[names(data)[status]]==1,]
+    X <- data.frame(D=datalong$dose/(1+params[1]*datalong$dose), model.matrix(~factor(loc)-1, data=datalong)[,-1], datalong[, names(data)[corrvars], drop=FALSE])
+  } else if(ccmethod=="meandose"){
+    datalong <- data[,-c(doses)]
+    datalong$dose <- rowMeans(data[,doses])
+    X <- data.frame(D=datalong$dose/(1+params[1]*datalong$dose), datalong[, names(data)[corrvars], drop=FALSE])
+  }
+
+  S0long <- (1+params[1]*datalong$dose)*exp(as.matrix(datalong[,names(data)[corrvars], drop=FALSE])%*%delta)
+  if(ccmethod!="meandose") S0long <- S0long*exp(model.matrix(~factor(loc)-1, data=datalong)%*%alpha)
+  S0 <- as.data.frame(data.table(S0long)[, list(S0=sum(V1)), by=list(set=datalong[,names(data)[set]])]) #aggregate(list(S0=S0long),by=list(set=datalong[,names(data)[set]]), FUN=sum)
+
+  #SX <- lapply(X, function(x) as.data.frame(data.table(x*S0long)[, list(SX=sum(V1)), by=list(set=datalong[,names(data)[set]])]))#aggregate(list(SX=x*S0long), by=list(set=datalong[,names(data)[set]]),FUN=sum))
+
+  SX <- sapply(X, function(x) {
+    tmp <- as.data.frame(data.table(x*S0long)[, list(SX=sum(V1)), by=list(set=datalong[,names(data)[set]])])
+    tmp$SX[match(datalong[,names(data)[set]], tmp$set)]
+  })
+
+
+  # matlist <- split(X, datalong[,names(data)[set]])
+  # matlist <- lapply(matlist, as.matrix)
+  #
+  # matlist2 <- split(S0long, datalong[,names(data)[set]])
+  # matlist2 <- lapply(matlist2, function(x) matrix(rep(x, each=ncol(X)), ncol=ncol(X),byrow=TRUE))
+  # diagmat2 <- bdiag(matlist2)
+  #
+  # diagmat <- bdiag(matlist)
+  #
+  # diagmat <- t(diagmat)%*%(diagmat*diagmat2)
+  #
+  #
+  #
+  # Avec <- c(1,rep(0, ncol(X)-1))
+  # Amat0 <- bdiag(lapply(1:length(matlist), function(x) matrix(Avec, nrow=1)))
+
+  SXX <- lapply(1:ncol(X),  function(x) matrix(nrow=nrow(datalong),ncol=ncol(X)))
+  #setnrlong <- data.table(set=datalong[,names(data)[set]])
+  dt1 <- data.table(X*S0long)
+  for(c1 in 1:ncol(X)){
+    #Amat <- cbind(matrix(0, nrow=nrow(Amat0), ncol=c1-1), Amat0[,1:(ncol(Amat0)-c1+1)])
+
+    #for(c2 in c1:ncol(X)){
+    #SXX[[c1]][[c2]] <- SXX[[c2]][[c1]] <- as.data.frame(data.table(X[,c1]*X[,c2]*S0long)[, list(SXX=sum(V1)), by=list(set=datalong[,names(data)[set]])])
+    #SXX[[c1]][[c2]] <- SXX[[c2]][[c1]] <- data.frame(set=unique(datalong[,names(data)[set]]), SXX=diagmat[cbind(c1+(1:length(matlist)-1)*ncol(X),c2+(1:length(matlist)-1)*ncol(X))])
+
+    #tmp <- as.data.frame(data.table(X[,c1]*X[,c2]*S0long)[, list(SXX=sum(V1)), by=list(set=datalong[,names(data)[set]])])
+    #SXX[[c1]][,c2] <- SXX[[c2]][,c1] <- tmp$SXX[match(datalong[,names(data)[set]], tmp$set)]
+
+    tmp <- data.table(set=datalong[,names(data)[set]],X[,c1]*dt1)[,(names(X)):=lapply(.SD, sum), by="set"]
+    #
+    #
+    SXX[[c1]] <- as.matrix(tmp[,-1])
+
+    # Bvec <- rep(0, ncol(X))
+    # Bvec[c2] <- 1
+    # Bmat <- matrix(rep(Bvec, length(matlist)), ncol=1)
+    #
+    # #SXX[[c1]][[c2]] <- SXX[[c2]][[c1]] <- data.frame(set=unique(datalong[,names(data)[set]]), SXX= as.numeric(Amat%*%diagmat%*%Bmat))
+    #
+    # tmp <- data.frame(set=unique(datalong[,names(data)[set]]), SXX= as.numeric(Amat%*%diagmat%*%Bmat))
+    #
+    # SXX[[c1]][,c2] <- SXX[[c2]][,c1] <- tmp$SXX[match(datalong[,names(data)[set]], tmp$set)]
+    #
+    # #SXX[[c1]][[c2]] <- SXX[[c2]][[c1]] <- setnrlong[tmp, on='set']$SXX
+
+    #}
+  }
+  # SXX <- lapply(X, function(x) {
+  #   lapply(X, function(y){
+  #     as.data.frame(data.table(x*y*S0long)[, list(SXX=sum(V1)), by=list(set=datalong[,names(data)[set]])])
+  #     #data.frame(set=datalong[,names(data)[set]],SXX=x*y*S0long) %>% group_by(set) %>% summarise(SXX=sum(SXX), .groups="drop")
+  #     #aggregate(list(SXX=x*y*S0long), by=list(set=datalong[,names(data)[set]]),FUN=sum)
+  #   })
+  # })
+  S0setlong <- S0$S0[match(datalong[,names(data)[set]], S0$set)]
+  probs <- S0long/S0setlong
+
+  Ulong <- sapply(1:ncol(X), function(x) X[,x]-SX[,x]/S0setlong)
+  if(repar) Ulong[,1] <- Ulong[,1]*params[1]
+
+  # U2long <- lapply(1:ncol(X), function(x){
+  #   sapply(1:ncol(X), function(y) (SX[,x]*SX[,y]-S0setlong*SXX[[x]][,y])/S0setlong^2)
+  # })
+
+  U2long <- lapply(1:ncol(X), function(x){
+    (SX[,x]*SX-S0setlong*SXX[[x]])/S0setlong^2
+    #sapply(1:ncol(X), function(y) (SX[,x]*SX[,y]-S0setlong*SXX[[x]][,y])/S0setlong^2)
+  })
+
+
+  if(ncol(X)>1){
+    for(ucol in 2:ncol(X)){
+      U2long[[1]][,ucol] <- ifelse(repar,params[1],1)*U2long[[1]][,ucol] # Needs to be multiplied with exp(xi) when repar=TRUE
+      U2long[[ucol]][,1] <- ifelse(repar,params[1],1)*U2long[[ucol]][,1] # Needs to be multiplied with exp(xi) when repar=TRUE
+    }
+  }
+  U2long[[1]][,1] <- ifelse(repar,params[1],0)*(X[,1]-SX[,1]/S0setlong)+ifelse(repar, params[1]^2,1)*(SX[,1]^2/S0setlong^2-X[,1]^2)
+
+
+  if(ccmethod!="meandose"){
+    #U <- lapply(Ulong, function(x) x[datalong$loc==datalong[,names(data)[loc]] & datalong[,names(data)[status]]==1])
+    U <- Ulong[datalong$loc==datalong[,names(data)[loc]] & datalong[,names(data)[status]]==1,,drop=FALSE]
+    U2 <- lapply(U2long, function(x) {
+      x[datalong$loc==datalong[,names(data)[loc]] & datalong[,names(data)[status]]==1,,drop=FALSE]}
+    )
+  } else {
+    #U <- lapply(Ulong, function(x) x[datalong[,names(data)[status]]==1])
+    U <- Ulong[datalong[,names(data)[status]]==1,]
+    U2 <- lapply(U2long, function(x) {
+      y[datalong[,names(data)[status]]==1,,drop=FALSE]}
+    )
+  }
+
+  EU2 <- sapply(U2long, function(x){
+    apply(x,2,function(i) sum(i*probs))
+  })
+
+  Umat <- Ulong
+
+  Umat2 <- Umat*matrix(rep(probs,each=ncol(X)), ncol=ncol(X),byrow=TRUE)
+
+  EUU <- t(Umat) %*% Umat2
+
+
+
+  # EUU2 <- lapply(Ulong, function(x){
+  #   sapply(U2long, function(y){
+  #     unname(sapply(as.data.frame(y), function(z) sum(x*z*probs)))
+  #   })
+  # })
+  #probslong <- matrix(rep(probs,each=ncol(X)),ncol=ncol(X),byrow=TRUE)
+
+  # EUU2 <- lapply(1:ncol(X), function(x){
+  #   sapply(U2long, function(y){
+  #     t(Ulong[,x]*probs)%*%(y)
+  #   })
+  # })
+
+  EUU2 <- lapply(U2long, function(x){
+    t(Ulong*as.numeric(probs))%*%x
+  })
+
+  EUU2 <- lapply(1:ncol(X), function(x){
+    sapply(EUU2, function(y) y[x,])
+  })
+
+
+
+
+  EUUU <- lapply(1:ncol(X), function(x){
+    t(Umat) %*% (Umat2*Ulong[,x])
+  })
+
+  U <- colSums(U)
+  U2 <- matrix(sapply(U2, function(x) colSums(x)), ncol=length(params))
 
   infomat <- solve(EUU)
 
@@ -285,13 +235,11 @@ linERRscore <- function(params,data, doses, set, status, loc, ccmethod, corrvars
     res <- 0
     for(s in 1:length(params)){
       for(t in 1:length(params)){
-        for(u in 1:length(params)){
-          for(v in 1:length(params)){
-            res <- res + U2[r,s]*infomat[s,t]*infomat[u,v]*(EUUU[t,u,v]+EUU2[t,u,v])
-          }
-        }
+        res <- res + U2[r,s]*infomat[s,t]*sum(infomat*(EUUU[[t]]+EUU2[[t]]))
+        #res <- res + U2[r,s]*infomat[s,t]*sum(infomat*(EUUU[[t]]+sapply(EUU2, function(x) x[,t])))
       }
     }
+
     A[r] <- -res/2
 
   }
